e53b6ff8f225b5d2a7cc668c921906c0c100e8f5
SheikhKiron <sheikhkiron59@gmail.com>
2025-11-26 18:26:52 +0600

    add firbase

--- src/app/Auth/AuthProvide.jsx
+++ src/app/Auth/AuthProvide.jsx
@@ -1,26 +1,28 @@
-
 'use client';
-import { createContext, useEffect, useState } from 'react';
-import { auth } from '@/Firebase/firebase.config';
+import { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { auth } from '../Firebase/firebase.config';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
-
-export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-
+  const [loading, setLoading] = useState(true);
+  const handleLogut = () => {
+    return signOut(auth);
+  };
   useEffect(() => {
-    if (!auth) return; 
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
-
-  const handleLogout = () => {
-    if (!auth) return;
-    return signOut(auth);
+  const authInfo = {
+    user,
+    handleLogut,
+    loading,
   };
-
-  return <AuthContext.Provider value={{ user, handleLogout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 }
